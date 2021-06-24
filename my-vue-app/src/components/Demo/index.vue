641b6b5b18ac4c425013d4209c8d47c7c20b903b
CodeHare <2835298021@qq.com>
2021-06-24 16:54:31 +0800

    first commit the code

---
+++ my-vue-app/src/components/Demo/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+    msg:  String
+    
+})
+
+const state = reactive({});
+
+// 组合式 API 的生命周期
+// 注：beforeCreate和created已经setup替代
+// 挂载前阶段
+onBeforeMount(() => {
+    // your code
+}),
+
+// 挂载阶段
+onMounted(() => {
+    
+}),
+
+// 更新前阶段
+onBeforeUpdate(() => {
+    
+}),
+
+// 更新阶段
+onUpdated(() => {
+    
+}),
+
+// 卸载前阶段
+onBeforeUnmount(() => {
+    
+}),
+
+// 卸载阶段
+onUnmounted(() => {
+    
+}),
+
+// 捕获错误
+onErrorCaptured(() => {
+    
+}),
+
+// 渲染
+onRenderTracked(() => {
+    
+}),
+
+// 触发渲染阶段
+onRenderTriggered(() => {
+    
+}),
+</script>
+
+<style module lang="scss">
+     
+</style>
---
+++ my-vue-app/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button type="button" @click="state.count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script setup>
+// 组合api用法
+// import { defineProps, reactive } from 'vue'
+import { defineProps } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+// 响应式的状态值第一种写法
+// const state = reactive({ count: 0 })
+
+// 响应式的值不引 reactive的第二种写法
+ref: count = 1000
+
+
+//  生命周期挂载
+ onMounted: {
+  count = 66
+  console.log("count是:", count)
+}
+
+</script> 
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
